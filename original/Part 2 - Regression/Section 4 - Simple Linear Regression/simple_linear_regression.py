ed73ab8957dc390143464343a45693db18efa5a9
ccarratala <167999595+ccarratala@users.noreply.github.com>
2024-09-12 16:57:14 +0200

    Update simple_linear_regression.py

--- original/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ original/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  1 12:07:43 2019
-
-@author: juangabriel
-"""
-
 # Regresión Lineal Simple
 
 # Cómo importar las librerías
@@ -15,44 +7,46 @@ import pandas as pd
 
 # Importar el data set
 dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+X = dataset.iloc[:, :-1].values # todas las col menos la ultima
+y = dataset.iloc[:, 1].values   # solo la ultima col (valor a predecir)
 
 
 # Dividir el data set en conjunto de entrenamiento y conjunto de testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+# lo dividimos 20 train y 10 test (N=30)
+
+'''Escalado de variables NO es necesario si es R. Lineal Simple'''
 
 
-# Escalado de variables
-"""
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-"""
 
 # Crear modelo de Regresión Lienal Simple con el conjunto de entrenamiento
 from sklearn.linear_model import LinearRegression
 regression = LinearRegression()
+# le pasamos los train set para ajustar el modelo .fit
 regression.fit(X_train, y_train)
 
-# Predecir el conjunto de test
+# Predecir el conjunto de test (crea nueva var con los valores pred)
 y_pred = regression.predict(X_test)
 
+
 # Visualizar los resultados de entrenamiento
+# datos reales
 plt.scatter(X_train, y_train, color = "red")
+# representamos la recta de regresion
 plt.plot(X_train, regression.predict(X_train), color = "blue")
 plt.title("Sueldo vs Años de Experiencia (Conjunto de Entrenamiento)")
 plt.xlabel("Años de Experiencia")
 plt.ylabel("Sueldo (en $)")
 plt.show()
 
+
 # Visualizar los resultados de test
+# controlar si hay overfitting (que no se ajuste bien al test_set)
 plt.scatter(X_test, y_test, color = "red")
+# la recta de la regresion es la misma 
 plt.plot(X_train, regression.predict(X_train), color = "blue")
 plt.title("Sueldo vs Años de Experiencia (Conjunto de Testing)")
 plt.xlabel("Años de Experiencia")
 plt.ylabel("Sueldo (en $)")
 plt.show()
-
