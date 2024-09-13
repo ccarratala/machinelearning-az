1a05012aad9088a16bf7a5f24f90554594784caa
ccarratala <167999595+ccarratala@users.noreply.github.com>
2024-09-13 12:01:37 +0200

    Update logistic_regression.py

--- original/Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.py
+++ original/Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 12 13:50:03 2019
-
-@author: juangabriel
-"""
-
 # Regresión Logística
 
 # Cómo importar las librerías
@@ -16,8 +8,8 @@ import pandas as pd
 # Importar el data set
 dataset = pd.read_csv('Social_Network_Ads.csv')
 
-X = dataset.iloc[:, [2,3]].values
-y = dataset.iloc[:, 4].values
+X = dataset.iloc[:, [2,3]].values   # edad y salario
+y = dataset.iloc[:, 4].values    # purchased Y/N
 
 
 # Dividir el data set en conjunto de entrenamiento y conjunto de testing
@@ -25,34 +17,43 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 
-# Escalado de variables
+# Escalado de variables: para que las variables estén en la misma escala
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
 
-# Ajustar el modelo de Regresión Logística en el Conjunto de Entrenamiento
+# Ajustar el modelo de Regresión Logística con el train_set
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
-# Predicción de los resultados con el Conjunto de Testing
-y_pred  = classifier.predict(X_test)
+# Predicción de los resultados con el test_set
+y_pred = classifier.predict(X_test)
+
 
 # Elaborar una matriz de confusión
+# para evaluar si las predicciones son correctas o no
+# falsos positivos y negativos
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred)    # le pasamos las etiquetas originales y las predicciones
+print(cm)  # para ver los resultados
+
 
 # Representación gráfica de los resultados del algoritmo en el Conjunto de Entrenamiento
 from matplotlib.colors import ListedColormap
+# esto es como hacer un hetmap de dos colores
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+# 'malla' de pixeles de la region de dibujo muy juntos (0.01)
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+# le pasamos las coordenadas X1 y X2, ademas de los colores con los que tiene que pintar las zonas
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
+# ciclo para colorear y mapear cada dato según su etiqueta 
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
