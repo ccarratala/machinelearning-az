cffc5a483bf46a7baa577b2c93c53fd48fc61cd3
ccarratala <167999595+ccarratala@users.noreply.github.com>
2024-09-16 10:59:43 +0200

    Update decision_tree_classification.py

--- original/Part 3 - Classification/Section 19 - Decision Tree Classification/decision_tree_classification.py
+++ original/Part 3 - Classification/Section 19 - Decision Tree Classification/decision_tree_classification.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 23 10:24:47 2019
-
-@author: juangabriel
-"""
-
 # Clasificación con árboles de Decisión
 
 
@@ -16,14 +8,14 @@ import pandas as pd
 
 # Importar el data set
 dataset = pd.read_csv('Social_Network_Ads.csv')
-
 X = dataset.iloc[:, [2,3]].values
 y = dataset.iloc[:, 4].values
-
 
 # Dividir el data set en conjunto de entrenamiento y conjunto de testing
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+''' No se escalan los datos porque la división no se hace con distancias '''
 
 
 # Ajustar el clasificador de Árbol de Decisión en el Conjunto de Entrenamiento
@@ -38,12 +30,15 @@ y_pred  = classifier.predict(X_test)
 # Elaborar una matriz de confusión
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
 
 # Representación gráfica de los resultados del algoritmo en el Conjunto de Entrenamiento
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 1),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 500))
+# se cambia el step porque no hemos escalado los datos (de año en año / de 500$ a 500$)
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
