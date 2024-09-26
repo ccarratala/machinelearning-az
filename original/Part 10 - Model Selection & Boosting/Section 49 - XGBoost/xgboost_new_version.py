b99fa469eecd33e54992a2d1dd52c0aaccfb9c03
ccarratala <167999595+ccarratala@users.noreply.github.com>
2024-09-26 13:24:39 +0200

    Update xgboost_new_version.py

--- original/Part 10 - Model Selection & Boosting/Section 49 - XGBoost/xgboost_new_version.py
+++ original/Part 10 - Model Selection & Boosting/Section 49 - XGBoost/xgboost_new_version.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May  5 09:58:24 2019
-
-@author: juangabriel
-"""
-
 # XGBoost
+
 # Las instrucciones de instalación se pueden consultar en http://xgboost.readthedocs.io/en/latest/build.html
+# pip install xgboost==1.1.0
+
 
 # Cómo importar las librerías
 import numpy as np
@@ -16,9 +11,9 @@ import pandas as pd
 
 # Importar el data set
 dataset = pd.read_csv('Churn_Modelling.csv')
-
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
+
 
 # Codificar datos categóricos
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -40,10 +35,12 @@ X = X[:, 1:]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+
 # Ajustar el modelo XGBoost al Conjunto de Entrenamiento
 from xgboost import XGBClassifier
 classifier = XGBClassifier()
 classifier.fit(X_train, y_train)
+
 
 # Predicción de los resultados con el Conjunto de Testing
 y_pred  = classifier.predict(X_test)
@@ -51,6 +48,7 @@ y_pred  = classifier.predict(X_test)
 # Elaborar una matriz de confusión
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
 
 # Aplicar k-fold cross validation
 from sklearn.model_selection import cross_val_score
